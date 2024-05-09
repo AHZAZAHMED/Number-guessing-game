574c9e4f5b2ca40ea77970094e59f6f361b9e027
AHZAZAHMED <160262331+AHZAZAHMED@users.noreply.github.com>
2024-05-09 16:54:57 +0500

    just done some correction

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 let randomNumber = Math.floor(Math.random() * 10);
-console.log(randomNumber);
 console.log(`you have three chance. 
 please guess the number between 1-10`);
 for (let i = 0; i <= 3; i++) {
