5e93bc5e7c51f9eca0e406a373d3f6a522e33212
AHZAZAHMED <160262331+AHZAZAHMED@users.noreply.github.com>
2024-05-09 16:59:32 +0500

    just done some correction

--- index.ts
+++ index.ts
@@ -2,14 +2,13 @@
 import inquirer from "inquirer";
 
 let randomNumber = Math.floor(Math.random() * 10);
-console.log(`you have three chance. 
-please guess the number between 1-10`);
-for (let i = 0; i <= 3; i++) {
+console.log(`you have three chance. `);
+for (let i = 0; i < 3; i++) {
   const answer = await inquirer.prompt([
     {
       name: "number",
       type: "number",
-      message: "",
+      message: "please guess the number between 1-10",
     },
   ]);
 
