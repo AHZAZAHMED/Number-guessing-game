2baf01038749a6060e7285922c075a5732359187
AHZAZAHMED <160262331+AHZAZAHMED@users.noreply.github.com>
2024-05-09 16:53:02 +0500

    used a for loop

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 import inquirer from "inquirer";
 let randomNumber = Math.floor(Math.random() * 10);
 console.log(randomNumber);
+console.log(`you have three chance. 
+please guess the number between 1-10`);
 for (let i = 0; i <= 3; i++) {
     const answer = await inquirer.prompt([
         {
             name: "number",
             type: "number",
-            message: "please guess the number between 1-10:",
+            message: "",
         },
     ]);
     if (answer.number === randomNumber) {
