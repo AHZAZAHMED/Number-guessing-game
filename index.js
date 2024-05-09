82450edf525ff1c02b3ffe31daee4dd97dd3c270
AHZAZAHMED <160262331+AHZAZAHMED@users.noreply.github.com>
2024-05-09 16:52:37 +0500

    use a for loop

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 let randomNumber = Math.floor(Math.random() * 10);
-const answer = await inquirer.prompt([
-    {
-        name: "number",
-        type: "number",
-        message: "please guess the number between 1-10:",
-    },
-]);
-if (answer.number === randomNumber) {
-    console.log("Congratulation! You have guess the rigth number.");
-}
-else {
-    console.log("you guess the wrong number");
+console.log(randomNumber);
+for (let i = 0; i <= 3; i++) {
+    const answer = await inquirer.prompt([
+        {
+            name: "number",
+            type: "number",
+            message: "please guess the number between 1-10:",
+        },
+    ]);
+    if (answer.number === randomNumber) {
+        console.log("Congratulation! You have guess the rigth number.");
+        break;
+    }
+    else {
+        console.log("you guess the wrong number");
+    }
 }
